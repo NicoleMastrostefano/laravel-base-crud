92662992721ebe24a28c483bc2435a2e9a347825
Nicole Mastrostefano <nicole_mas@hotmail.it>
2021-02-10 17:30:58 +0100

    initial commit

---
+++ database/migrations/2021_02_10_143324_create_beers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBeersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('beers', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome',30);
+            $table->string('marca',20);
+            $table->float('prezzo',4,2);
+            $table->unsignedDecimal('Alc/Vol', 2, 1);
+            $table->string('descrizione',200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('beers');
+    }
+}
---
+++ resources/views/beers/index.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    </head>
+    <body>
+      <main id="container">
+        <h1>I nostri libri</h1>
+        <table class="table table-dark table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nome</th>
+              <th>Marca</th>
+              <th>Prezzo</th>
+              <th>alc vol</th>
+              <th>Descrizione</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach ($beers as $beer)
+            <tr>
+              <td> {{ $beer->id }}</td>
+              <td> {{ $beer->nome }}</td>
+              <td> {{ $beer->marca }}</td>
+              <td> {{ $beer->prezzo }}</td>
+              <td> {{ $beer->Alc/Vol }}</td>
+              <td> {{ $beer->descrizione }}</td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </main>
+
+    </body>
+</html>
